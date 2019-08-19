43eac029aa198b9c9391b82f1edddb6760f4979e
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-19 12:11:58 -0400

    updated modules

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 import "bootstrap";
-import "../actions/clicks";
 import "../actions/hover";
 import "../components/card-projects";
 import "../actions/filter";
-
+import "../actions/scroll";
+import "../actions/input";

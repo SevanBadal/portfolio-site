d456292b4725944f04e875ae42a17268beb09096
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-19 12:59:05 -0400

    p tag in banner and link container

--- app/javascript/components/card-projects.js
+++ app/javascript/components/card-projects.js
@@ -7,9 +7,11 @@ export const addCard = ({ name, created_at, description, html_url }) => {
         <h5 class='card-title'>${name}</h5>
         <h6 class='card-subtitle text-muted'>${formattedDate}</h6>
         <p class='card-text'>${description}</p>
-        <a href='${html_url}' target='_default' class='card-link'>
-          <i class='fas fa-code-branch'></i>
-        </a>
+        <div class="link-container">
+          <a href='${html_url}' target='_default' class='card-link'>
+            <i class='fas fa-code-branch'></i>
+          </a>
+        </div>
       </div>
     </div>`;
   projectRoot.insertAdjacentHTML("beforeend", card);

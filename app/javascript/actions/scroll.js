a4623ca416c07577892468d2ede10df529e8e326
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-19 16:51:48 -0400

    auto scroll up

--- app/javascript/actions/scroll.js
+++ app/javascript/actions/scroll.js
@@ -3,12 +3,12 @@ import { scrollScreen } from '../actions/clicks';
 function debounce(func, wait, immediate) {
   let timeout;
   return function () {
-    let context = this, args = arguments;
-    let later = function () {
+    const context = this, args = arguments;
+    const later = function () {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
-    let callNow = immediate && !timeout;
+    const callNow = immediate && !timeout;
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
@@ -16,13 +16,15 @@ function debounce(func, wait, immediate) {
 };
 
 const myEfficientFn = debounce(function (e) {
-    // print "false" if direction is down and "true" if up
   const bottomHalf = document.getElementById('bottom-half');
   const offset = bottomHalf.getBoundingClientRect().top
     if(offset >= 0) {
       this.oldScroll < this.scrollY && scrollScreen('bottom-half');
-      this.oldScroll = this.scrollY;
     }
-}, 50);
+    if (offset >= 100) {
+      this.oldScroll > this.scrollY && scrollScreen('top-half');
+    }
+    this.oldScroll = this.scrollY;
+}, 75);
 
 window.addEventListener('scroll', myEfficientFn);

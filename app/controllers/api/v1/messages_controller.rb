40df71cd46152bcc5554f3c1f025c82f1b54644e
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-21 22:07:14 -0400

    action mailer setup

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,10 @@
 class Api::V1::MessagesController < Api::V1::BaseController
   def create
     @message = Message.new({name: params[:name], text: params[:text], email: params[:email]})
+    puts @message
     if @message.save
+      # Tell the AdminMailer to send an email after save
+      AdminMailer.with(message: @message).new_message.deliver_now
       render json: {status: 'success'}
     else
       render_error

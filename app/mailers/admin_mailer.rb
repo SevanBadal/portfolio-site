fe9e1725182046ed8b87f32d4a0ba23455cdff13
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-21 22:12:28 -0400

    removed puts

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,8 +1,6 @@
 class AdminMailer < ApplicationMailer
   def new_message
     @message = params[:message]
-    p '*****************ADMIN***************************'
-    p params
     mail(to: 'sgolnaza@gmail.com', subject: 'Sevan Portfolio - New Message')
   end
 end
